b6260f5d556f29633047083d513c792924f1ca8c
Thomas KOEBEL <thomas.koebel@epitech.eu>
2024-04-17 15:20:02 +0200

    Update package.json, index.html, index.css, and portfolio/style.css files

--- src/content_option.js
+++ src/content_option.js
@@ -11,13 +11,13 @@ const introdata = {
         second: "Passionnée",
         third: "Créative",
     },
-    description: "Je suis Marion EYLER, étudiante en Motion Design et Filmmaker sur Strasbourg. J'ai toujours été passionnée par le cinéma et la photographie. J'ai donc décidé de me lancer dans cette voie. Je suis actuellement en 2ème année à E-artsup Strasbourg. Je suis à la recherche d'un stage de 3 mois en tant que monteuse, motion designer ou photographe au cours de l'année 2024.",
+    description: "Je m’appelle Marion EYLER, et je suis actuellement étudiante en troisième année de Motion Design et Filmmaking à e-artusup Strasbourg. Depuis toujours, ma passion pour le cinéma et la photographie m’anime. C’est pourquoi j’ai choisi de suivre cette voie. Je suis fascinée par le pouvoir narratif du cinéma et par la façon dont la photographie capture des moments uniques et les fige dans le temps. Chaque projet me donne l’opportunité d’approfondir mes compétences et d’explorer de nouvelles perspectives artistiques. Je suis déterminée à développer mon talent et à contribuer au monde du cinéma et de la photographie grâce à mon travail et à ma vision.",
     your_img_url: "/assets/profil.jpg",
 };
 
 const dataabout = {
     title: "Un peu plus sur moi",
-    aboutme: "Je suis Marion EYLER, étudiante en Motion Design et Filmaker sur Strasbourg. J'ai toujours été passionnée par le cinéma et la photographie. J'ai donc décidé de me lancer dans cette voie. Je suis actuellement en 2ème année à E-artsup Strasbourg. Je suis à la recherche d'un stage de 3 mois en tant que monteuse, motion designer ou photographe au cours de l'année 2024.",
+    aboutme: "Je m’appelle Marion EYLER, et je suis actuellement étudiante en troisième année de Motion Design et Filmmaking à e-artusup Strasbourg. Depuis toujours, ma passion pour le cinéma et la photographie m’anime. C’est pourquoi j’ai choisi de suivre cette voie. Je suis fascinée par le pouvoir narratif du cinéma et par la façon dont la photographie capture des moments uniques et les fige dans le temps. Chaque projet me donne l’opportunité d’approfondir mes compétences et d’explorer de nouvelles perspectives artistiques. Je suis déterminée à développer mon talent et à contribuer au monde du cinéma et de la photographie grâce à mon travail et à ma vision.",
 };
 const worktimeline = [
     {
@@ -74,85 +74,88 @@ const showreel = "/assets/showreel.mp4";
 const videosportfolio = [
     {
         content: "/assets/animrenard.mp4",
-        description: "",
+        title: "Le renard polaire",
+        description: "L’animation a été minutieusement créée à la main, image par image, réaliser sur Procreate. Chaque détail a été soigneusement élaboré pour donner vie à l’animation. Parallèlement, le fond a été réalisé par une collègue spécialisée en direction artistique, apportant une dimension supplémentaire à l’ensemble. Cette collaboration entre l’animation dessinée à la main et le fond conçu, crée une fusion harmonieuse d’éléments visuels, enrichissant ainsi l’esthétique globale du projet.",
     },
-    {
-        content: "/assets/intro.mp4",
-        description: "",
-    },
-    {
-        content: "/assets/planet.mp4",
-        description: "",
-    },
+    // {
+    //     content: "/assets/intro.mp4",
+    //     description: "",
+    // },
+    // {
+    //     content: "/assets/planet.mp4",
+    //     title: "Planète",
+    //     description: "",
+    // },
     {
         content: "/assets/l'ami du chat.mp4",
-        description: "",
-    },
-    {
-        content: "/assets/chambre_horror.mp4",
-        description: "",
+        title: "L'ami du chat",
+        description: "Cette animation a été réalisée en collaboration avec un partenaire. J’ai pris en charge l’animation du chat et de la gamelle, en créant chaque image manuellement, tandis que mon binôme s’est occupé des autres animations. Ensuite, j’ai assemblé le montage final. Cette animation raconte une histoire amusante et enfantine d’amitié entre une gamelle et un chat, illustrant leur relation de manière humoristique et légère...",
     },
 ]
 
 const photosportfolio = [
     {
-        content: "/assets/ref2.jpg",
-        description: "",
-        alt: "/assets/ref1.jpg",
+        // content: "/assets/ref2.jpg",
+        title: "Parfum",
+        description: "Dans le cadre d'un projet scolaire portant sur la photographie publicitaire d'objets, j'ai décidé d'appliquer la technique du clair-obscur pour mettre en avant certains éléments. Pour ce faire, j'ai opté pour une approche différente en plaçant l'objet dans l'obscurité totale. En utilisant une pose longue de mon appareil photo, j'ai eu l'opportunité de manipuler une source lumineuse pour éclairer l'objet selon mes préférences. Cette méthode m'a permis de contrôler avec précision l'éclairage et les ombres, offrant ainsi une mise en valeur ciblée de l'objet. En choisissant cette approche, j'ai pu explorer de nouvelles techniques de photographie publicitaire tout en exprimant ma créativité à travers la manipulation de la lumière pour obtenir le résultat désiré.",
+        alt: ["/assets/parfum1.jpg", "/assets/parfum2.jpg", "/assets/parfum3.jpg"],
     },
     {
-        content: "/assets/thomas1.jpg",
-        description: "",
+        title: "Portrait",
+        description: "J'ai réalisé un portrait en studio dans un style clair-obscur en utilisant un déclencheur spécial. Dans l'obscurité totale, j'ai utilisé ce déclencheur pour activer un projecteur au moment de la prise de vue, simulant ainsi un flash. Cette technique m'a permis de créer un effet clair-obscur saisissant, mettant en valeur les contrastes forts entre les zones lumineuses et les zones sombres du sujet. En exploitant habilement cette méthode, j'ai pu obtenir des jeux d'ombres et de lumières dramatiques, ajoutant une dimension supplémentaire à la composition du portrait.",
+        alt: ["/assets/thomas1.jpg", "/assets/thomas2.jpg", "/assets/intro1.jpg"],
     },
+   
     {
-        content: "/assets/thomas2.jpg",
-        description: "",
-    },
-    {
-        content: "/assets/intro1.jpg",
-        description: "",
-    },
-    {
-        content: "/assets/cerf.png",
-        description: "",
-    },
-    {
-        content: "/assets/kiki.jpg",
-        description: "",
-    },
-    {
-        content: "/assets/kiki-2.jpg",
-        description: "",
-    },
-    {
-        content: "/assets/kiki-3.jpg",
-        description: "",
-    },
-    {
-        content: "/assets/koi.png",
-        description: "",
-    },
-    {
-        content: "/assets/mains.png",
-        description: "",
-    },
-    {
-        content: "/assets/maison_horreur.png",
-        description: "",
-    },
-    {
-        content: "/assets/Marion_EYLER_Horror.png",
-        description: "",
+        title: "Un Chat",
+        description: "À travers une séance photo mettant en scène un chat, j'ai mis en lumière ma patience remarquable. L'objectif était de focaliser l'attention sur la tête du félin et ses expressions oculaires changeantes. En prenant le temps nécessaire, j'ai capturé des moments où le regard du chat reflète diverses émotions, soulignant ainsi sa nature fascinante et énigmatique. Cette démarche témoigne de mon engagement à saisir des instants uniques et à créer des images évocatrices. En choisissant de mettre en avant la tête du chat, j'ai cherché à évoquer sa personnalité et à susciter l'intérêt du spectateur pour son charme intriguant. Cette séance photo illustre non seulement ma patience et ma persévérance dans la capture d'images significatives, mais aussi ma capacité à créer des compositions visuelles qui captivent et intriguent. ",
+        alt: ["/assets/kiki.jpg","/assets/kiki-2.jpg", "/assets/kiki-3.jpg"],
     },
     {
         content: "/assets/papathomas.jpg",
-        description: "",
+        title: "Reflexion",
+        description: "La photographie capture un homme profondément absorbé dans une partie d'échecs, mettant en avant les pions blancs avec netteté tandis que les pions noirs sont flous, dirigant ainsi le regard vers le joueur en pleine réflexion. Une retouche colorimétrique a été appliquée pour insuffler une ambiance légèrement vintage à l'image, avec l'ajout subtil de grain. Cette manipulation renforce l'aspect intemporel de la scène, tout en évoquant une atmosphère nostalgique. L'effet de flou sélectif contribue à créer une focalisation sur l'élément principal de la composition, à savoir l'homme et son engagement dans le jeu d'échecs. Cette photographie transmet à la fois une impression de concentration intense et une esthétique rétro, capturant ainsi un moment figé dans le temps où la stratégie et la contemplation se mêlent dans une ambiance émotionnel",
     },
+    
+
+];
+
+const illustrationsportfolio = [
     {
         content: "/assets/sniper.png",
-        description: "",
+        description: "L’illustration adopte un style manga et met en scène la perspective d’un tireur d’élite observant un couple sur le point de s’embrasser. Dans cette scène chargée de tension, le spectateur est invité à se demander si le tireur va intervenir en tirant. La composition visuelle est soigneusement orchestrée pour accentuer le contraste entre l’intimité du couple et la menace imminente. Le couple, capturé au premier plan, semble être dans un moment d’intimité et de tendresse, ignorant complètement le danger qui les guette. En revanche, le tireur, rendu anonyme par l’absence de détails faciaux, est parfaitement camouflé et concentré sur sa cible.",
+        title: "Trahison",
     },
+    {
+        title: "Triptyque",
+        description: "Le triptyque réalisé sur Pro- create illustre la confrontation entre le feu et l’eau, symbo- lisant respectivement la des- truction et la vie. Au centre de l’œuvre, ces éléments s’affrontent tel un Yin Yang, démontrant l’interdépen- dance des forces opposées. La première section met en scène le feu dans toute sa puissance destructrice, avec de grandes flammes et une palette de couleurs chaudes évoquant la chaleur intense et le chaos. En contraste, la par- tie dédiée à l’eau présente des formes fluides et des teintes calmes, évoquant la tranquil- lité, la pureté et la vitalité.",
+        alt: ["/assets/triptyque.jpg", "/assets/cerf.png", "/assets/mains.png",  "/assets/koi.png"],
+    },
+    // {
+    //     content: "/assets/koi.png",
+    //     description: "",
+    // },
+    // {
+    //     content: "/assets/mains.png",
+    //     description: "",
+    // },
+    // {
+    //     content: "/assets/cerf.png",
+    //     description: "",
+    // },
+];
 
+const unrealportfolio = [
+    {
+        content: "/assets/Marion_EYLER_Horror.png",
+        description: "",
+        title: "Horror",
+    },
+        {
+        video: "/assets/chambre_horror.mp4",
+        title: "Chambre d'horreur",
+        description: "J'ai utilisé Unreal Engine et des assets pour créer une chambre d'enfant virtuelle. En animant divers objets présents dans la chambre ainsi que la caméra pour donner vie à l’ensemble et créer une animation immersive.  Ce projet met en valeur mes compétences en conception d'environnements virtuels et en animation sur Unreal Engine.",
+    },
 ];
 
 const contactConfig = {
@@ -178,6 +181,8 @@ export {
     dataabout,
     videosportfolio,
     photosportfolio,
+    illustrationsportfolio,
+    unrealportfolio,
     showreel,
     worktimeline,
     skills,
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
-import { videosportfolio, photosportfolio, showreel, meta } from "../../content_option";
+import { videosportfolio, photosportfolio, illustrationsportfolio, unrealportfolio, showreel, meta } from "../../content_option";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 
 export const Portfolio = () => {
 
@@ -44,6 +47,7 @@ export const Portfolio = () => {
               <div key={i} className="po_item">
                 <video src={process.env.PUBLIC_URL + data.content} muted autoPlay loop playsInline></video>
                 <div className="content">
+                  <h4>{data.title}</h4>
                   <p>{data.description}</p>
                   
                 </div>
@@ -63,9 +67,113 @@ export const Portfolio = () => {
             return (
               <div key={i} className="po_item">
                 <img src={process.env.PUBLIC_URL + data.content} alt="" />
+                {data.alt != null && (
+                  <Carousel
+                    showArrows={false}
+                    showThumbs={false}
+                    showStatus={false}
+                    infiniteLoop={true}
+                    autoPlay={true}
+                    interval={3000}
+                    className="carousel"
+                  >
+                    {data.alt.map((alt, i) => {
+                      return (
+                        <div key={i}>
+                          <img src={process.env.PUBLIC_URL + alt} alt="" />
+                        </div>
+                      );
+                    })}
+                  </Carousel>
+                )}
                 <div className="content">
-                  <p>{data.description}</p>
-                  <img src={process.env.PUBLIC_URL + data.alt} alt="" />
+                  <h4>{data.title}</h4>
+                  {data.description.length > 400 && (<p style={{fontSize: "16px"}}>{data.description}</p>)}
+                  {data.description.length <= 400 && (<p>{data.description}</p>)}
+                  {/* <img src={process.env.PUBLIC_URL + data.alt} alt="" /> */}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <Row className="mb-5">
+          <Col lg="8">
+            <h1 className="display-8 mb-4"> Illustrations </h1>{" "}
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <div className="mb-5 po_items_ho">
+          {illustrationsportfolio.map((data, i) => {
+            return (
+              <div key={i} className="po_item">
+                <img src={process.env.PUBLIC_URL + data.content} alt="" />
+                {data.alt != null && (
+                  <Carousel
+                    showArrows={false}
+                    showThumbs={false}
+                    showStatus={false}
+                    infiniteLoop={true}
+                    autoPlay={true}
+                    interval={3000}
+                    className="carousel"
+                  >
+                    {data.alt.map((alt, i) => {
+                      return (
+                        <div key={i} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                          <img src={process.env.PUBLIC_URL + alt} alt=""/>
+                        </div>
+                      );
+                    })}
+                  </Carousel>
+                )}
+                <div className="content">
+                  <h4>{data.title}</h4>
+                  {data.description.length > 400 && (<p style={{fontSize: "16px"}}>{data.description}</p>)}
+                  {data.description.length <= 400 && (<p>{data.description}</p>)}
+                  {/* <img src={process.env.PUBLIC_URL + data.alt} alt="" /> */}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <Row className="mb-5">
+          <Col lg="8">
+            <h1 className="display-8 mb-4"> Unreal</h1>{" "}
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <div className="mb-5 po_items_ho">
+          {unrealportfolio.map((data, i) => {
+            return (
+              <div key={i} className="po_item">
+                <img src={process.env.PUBLIC_URL + data.content} alt="" />
+                {data.video != null && (
+                  <video src={process.env.PUBLIC_URL + data.video} muted autoPlay loop playsInline></video>
+                )}
+                {data.alt != null && (
+                  <Carousel
+                    showArrows={false}
+                    showThumbs={false}
+                    showStatus={false}
+                    infiniteLoop={true}
+                    autoPlay={true}
+                    interval={3000}
+                    className="carousel"
+                  >
+                    {data.alt.map((alt, i) => {
+                      return (
+                        <div key={i} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                          <img src={process.env.PUBLIC_URL + alt} alt=""/>
+                        </div>
+                      );
+                    })}
+                  </Carousel>
+                )}
+                <div className="content">
+                  <h4>{data.title}</h4>
+                  {data.description.length > 400 && (<p style={{fontSize: "16px"}}>{data.description}</p>)}
+                  {data.description.length <= 400 && (<p>{data.description}</p>)}
+                  {/* <img src={process.env.PUBLIC_URL + data.alt} alt="" /> */}
                 </div>
               </div>
             );
